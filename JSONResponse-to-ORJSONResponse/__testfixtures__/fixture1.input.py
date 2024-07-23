a19f51c7daef1dc50b2128030114fd8885907400
Alex Bit <alex@codemod.com>
2024-07-23 15:24:59 -0700

    test fixtures updated.

--- JSONResponse-to-ORJSONResponse/__testfixtures__/fixture1.input.py
+++ JSONResponse-to-ORJSONResponse/__testfixtures__/fixture1.input.py
@@ -1,15 +1,16 @@
 from fastapi.responses import JSONResponse
+from typing import Callable, Dict, Any
 
-def default(o: object) -> object:
-    if isinstance(o, Money):
-        return {"amount": o.amount, "currency": o.currency.value}
-    
-class ORJSONResponse(JSONResponse):
-    def render(self, content: object) -> bytes:
-        return orjson.dumps(
-            content,
-            default=default,
-            option=orjson.OPT_PASSTHROUGH_DATETIME
-                   | orjson.OPT_NON_STR_KEYS
-                   | orjson.OPT_SERIALIZE_NUMPY,
-        )
+# Function that takes a callable returning JSONResponse
+def process_response(response_generator: Callable[[], JSONResponse]) -> JSONResponse:
+    response = response_generator()
+    # Further processing with response
+    return response
+
+# Example usage
+def generate_response() -> JSONResponse:
+    data = {"message": "Hello, world!"}
+    return JSONResponse(content=data)
+
+# Processing the generated response
+processed_response = process_response(generate_response)
--- JSONResponse-to-ORJSONResponse/__testfixtures__/fixture1.output.py
+++ JSONResponse-to-ORJSONResponse/__testfixtures__/fixture1.output.py
@@ -1,16 +1,15 @@
 # Note: importing from ORJSONResponse
 from src.json_encoding import ORJSONResponse
+from typing import Callable
 
-def default(o: object) -> object:
-    if isinstance(o, Money):
-        return {"amount": o.amount, "currency": o.currency.value}
-    
-class ORJSONResponse(ORJSONResponse):  # Note: Inheriting from ORJSONResponse
-    def render(self, content: object) -> bytes:
-        return orjson.dumps(
-            content,
-            default=default,
-            option=orjson.OPT_PASSTHROUGH_DATETIME
-                   | orjson.OPT_NON_STR_KEYS
-                   | orjson.OPT_SERIALIZE_NUMPY,
-        )
+# Note: Usage of ORJSONResponse
+def process_response(response_generator: Callable[[], ORJSONResponse]) -> ORJSONResponse:
+    response = response_generator()
+    return response
+
+# Note: Usage of ORJSONResponse
+def generate_response() -> ORJSONResponse:
+    data = {"message": "Hello, world!"}
+    return ORJSONResponse(content=data)
+
+processed_response = process_response(generate_response)
